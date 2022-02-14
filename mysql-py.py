af589c7dfdc90e2765c29ede058bd8942e930c12
Anas-jaf <ansas4565@gmail.com>
2022-02-14 04:17:05 +0200

    finished the scirpt that can insort text to db

--- mysql-py.py
+++ mysql-py.py
@@ -38,27 +38,29 @@ file_names2=os.listdir()
 # print(file_names2)
 # try:
 for x in file_names2:
-    folder_files = os.listdir(x)
-    
-    print (f'files\' folders of `{x}`')
-    kursor.execute(f" create  TABLE `{x}` (textID int NOT NULL AUTO_INCREMENT PRIMARY KEY, text varchar(255));" )
-    db_handle.commit()
-    
-    for y in folder_files:
-        file = open(f'{x}/{y}', 'r')
+    if x !=(".git") : 
+        folder_files = os.listdir(x)
+        
+        print (f'files\' folders of `{x}`')
+        kursor.execute(f" create  TABLE `{x}` (textID int NOT NULL AUTO_INCREMENT PRIMARY KEY, text varchar(255));" )
+        db_handle.commit()
+        
+        for y in folder_files:
+            file = open(f'{x}/{y}', 'r')
 
-        for z in file.readlines():
-            texts.append(z.rstrip('\n'))
-            
-        # print (f'`{y}`')
-        for i in texts:
-            kursor.execute(f"INSERT INTO {x} (text) VALUES (`{i}`);") 
-            db_handle.commit()
-            print (i)
-        # kursor.execute(f"INSERT INTO `{x}` (text) VALUES ('`{y}`'); " ) 
-        # db_handle.commit()            
-# except:
-#     print('#####################################')
-#     print (x)
-#     print('not a folder')
-# print (file_names2)
+            for z in file.readlines():
+                texts.append(z.rstrip('\n'))
+                
+            # print (f'`{y}`')
+            for i in texts:
+                # print(f"INSERT INTO {x} VALUES (\"\" , \'{i}\');")
+                kursor.execute(f"INSERT INTO `{x}` VALUES (\"\" , \'{i}\');") 
+                db_handle.commit()
+                # print (i)
+            # kursor.execute(f"INSERT INTO `{x}` (text) VALUES ('`{y}`'); " ) 
+            # db_handle.commit()            
+    # except:
+    #     print('#####################################')
+    #     print (x)
+    #     print('not a folder')
+    # print (file_names2)
