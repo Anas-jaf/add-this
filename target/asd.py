58d2cbc22073141e20bcb4c7524760b93734d7a8
Anas-jaf <ansas4565@gmail.com>
2022-03-07 23:14:50 +0300

    fixed mysql query

--- target/asd.py
+++ target/asd.py
@@ -35,31 +35,45 @@ print("######################################")
 
 # file_text = [os.system("powershell -c \"ls -n\"")]
 file_text2=os.listdir()
-# print(file_text2)
+print(file_text2)
 #try:
 for x in file_text2:
-    
-    folder_files = os.listdir(x)
-    # x=re.escape(x)
-    print (f'files\' folders of `{x}`##########################')
-    # print (x) 
-    # kursor.execute(f" create  TABLE `{x}` (textID int NOT NULL AUTO_INCREMENT PRIMARY KEY, text varchar(255));" )
-    # db_handle.commit()
-
-    for y in folder_files:
-        # y = re.escape(y)
-        # print (f'`{y}`')
+    if x != "asd.py":
+        # print(x)
+        folder_files = os.listdir(x)
+        # x=re.escape(x)
         
-        print (f'{x}/{y}***********')
-        file = open(f'{x}/{y}', 'r')
+        print (f'files\' folders of `{x}`##########################')
+        # print (x) 
+        # kursor.execute(f" create  TABLE `{x}` (textID int NOT NULL AUTO_INCREMENT PRIMARY KEY, text text);" )
+        kursor.execute(f" create  TABLE `{x}` ( textID int NOT NULL , text text);" )
+        db_handle.commit()
 
-        for z in file.readlines():
+        for y in folder_files:
+            # y = re.escape(y)
+            # print (f'`{y}`')
+            
+            print (f'{x}/{y}***********')
+            file = open(f'{x}/{y}', 'r')
 
-            text.append(z.rstrip('\n').replace('\xa0', ''))
-            print(text)
-        text = []
-        # kursor.execute(f"INSERT INTO `{x}` (text) VALUE (\'{y}\'); " ) 
-        # db_handle.commit()            
+            for z in file.readlines():
+
+                # text.append(z.rstrip('\n').replace('\xa0', '').replace('\'', '').replace('\"', ''))
+                no_SC=z.replace('\'', "\\'").replace('\"', '\\"')
+                noSC2=re.escape(z)
+                noSC3=noSC2.replace('\\ ', ' ')
+
+                table = str.maketrans({"-":  r"\-", "]":  r"\]", "\\": r"\\",
+                                    "^":  r"\^", "$":  r"\$", "*":  r"\*", ".":  r"\." , "'": r"\'" , '"' : r"\""})
+                # Replace string
+                s_new = z.translate(table)
+                y=y.replace("_video_or_play_page.txt", "").replace(".txt" , "")
+                print(y)
+                print(f'{s_new}__________________')
+                kursor.execute(f"INSERT INTO `{x}` VALUE ({y},'{s_new}'); " ) 
+                db_handle.commit()         
+                # input("continue")   
+            text = []
 #except:
 #    print('#####################################')
 #    print (x)
